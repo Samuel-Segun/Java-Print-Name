17af4d3db3b4c69f74e57b153a8500abcf8e20cb
Samuel-Segun <oyedokunsamuelsegun@gmail.com>
2022-06-23 01:03:30 +0100

    javascript print name

---
+++ life.js
@@ -0,0 +1,6 @@
+const name= "OYEDOKUN SAMUEL SEGUN";
+ const height= "5.2ft tall";
+ const country= "Nigeria";
+ 
+ 
+    alert("\n NAME: " + name + " \n \n \n" + "HEIGHT: " + height + "\n \n \n" + "COUNTRY: " + country);
